bd7fd8c8579be77ac7f5b56679d934a158efbe59
shadid12 <shadidhaque2014@gmail.com>
2018-02-24 01:59:54 -0500

    Initial commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import './css/home.css';
+
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            pass: '',
+            disabled: false
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handlePass = this.handlePass.bind(this);
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        if(this.state.pass && this.state.disabled) {
+            this.setState({disabled: false})
+        }
+    }
+
+    handleChange(e) {
+        this.setState({email: e.target.value});
+        if(this.state.email && this.state.pass) {
+            this.setState({ disabled: false })
+        }
+    }
+
+    handlePass(e) {
+        this.setState({pass: e.target.value});
+        if(this.state.email && this.state.pass) {
+            this.setState({ disabled: false })
+        }
+    }
+
+    render() {
+
+
+        return (
+            <div className='container'>
+                <div className='top'></div>
+                <div className='card'>
+                    <MuiThemeProvider>
+                        <Card >
+                            <div className='wrapper'>
+                                <TextField
+                                    hintText="Email"
+                                    value={this.state.email} onChange={this.handleChange}
+                                /><br/>
+                                <TextField
+                                    hintText="Password"
+                                    type="password"
+                                /><br/>
+                                <div className='login-btn'>
+                                    <RaisedButton label="Login" primary={true}
+                                                  disabled={this.state.disabled} />
+                                </div>
+                            </div>
+                        </Card>
+                    </MuiThemeProvider>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
